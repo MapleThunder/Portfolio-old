a4813e031af82584407ebc71a8aef910350d4910
Niko Bentley <bentley@curingresin.com>
2017-03-11 20:42:59 -0400

    Added skills and a background

--- resources/views/Pages/skills.blade.php
+++ resources/views/Pages/skills.blade.php
@@ -3,15 +3,34 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Skills</div>
+            <div class="col-md-4">
+                <h3>Software</h3>
+                <img class="skill-img" src="img/skills/vs.jpg" />
+                <img class="skill-img" src="img/skills/eclipse.png" />
+                <img class="skill-img" src="img/skills/phpstorm.png" />
+                <img class="skill-img" src="img/skills/xamarin.png" />
+                <img class="skill-img" src="img/skills/oracle.jpg" />
+                <img class="skill-img" src="img/skills/adobe.jpg" />
+                <img class="skill-img" src="img/skills/mysql.jpg" />
+                <img class="skill-img" src="img/skills/ms-sqlsvr.jpg" />
+                <img class="skill-img" src="img/skills/android-studio.jpg" />
+            </div>
 
-                    <div class="panel-body">
-                        MAD SKILLZ WILL GO HERE
+            <div class="col-md-4">
+                <h3>Programming</h3>
+                <img class="skill-img" src="img/skills/csharp.png" />
+                <img class="skill-img" src="img/skills/python.png" />
+                <img class="skill-img" src="img/skills/java.png" />
+                <img class="skill-img" src="img/skills/ms-net.png" />
+            </div>
 
-                    </div>
-                </div>
+            <div class="col-md-4">
+                <h3>Web</h3>
+                <img class="skill-img" src="img/skills/phalcon.png" />
+                <img class="skill-img" src="img/skills/php.png" />
+                <img class="skill-img" src="img/skills/jquery.png" />
+                <img class="skill-img" src="img/skills/laravel.png" />
+                <img class="skill-img" src="img/skills/web-langs.png" />
             </div>
         </div>
     </div>
